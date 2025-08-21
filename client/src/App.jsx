8e2d15b94593a89090863d83bc813dd01d7bd651
Patchara <sombrousnorn@gmail.com>
2025-08-21 10:25:26 +0700

    feat:update vite and tailwindcdd @latest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      {/* Start coding here */}
+      <h1 className="bg-red-500 text-2xl font-bold text-red-500">Tourist Attraction</h1>
+      <p className="text-lg">Tourist Attraction is a place that is visited by many people.</p>
+      <p className="text-lg">Tourist Attraction is a place that is visited by many people.</p>
+    </div>
+  );
 }
 
 export default App;
